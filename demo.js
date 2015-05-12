80995c7cd3319698fd58244b6bd93c726a46eba9
Christopher Van <cvan@mozilla.com>
2015-05-12 00:19:09 -0700

    hook up gamepad-plus (for polyfilled events and helpers)

--- demo.js
+++ demo.js
@@ -12,36 +12,126 @@
   }
 });
 
-exports.addEventListener('gamepadconnected', function (e) {
-  console.log('Gamepad connected at index %d: %s. %d buttons, %d axes.',
-    e.gamepad.index, e.gamepad.id, e.gamepad.buttons.length, e.gamepad.axes.length);
-});
 
-exports.addEventListener('gamepaddisconnected', function (e) {
-  console.log('Gamepad removed at index %d: %s.', e.gamepad.index, e.gamepad.id);
-});
+var raf = window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame;
 
-exports.addEventListener('gamepadaxismove', function (e) {
-  gcursor.onGamepadAxisMove(e);
-  gscroll.onGamepadAxisMove(e);
+if (!raf) {
+  throw 'requestAnimationFrame is required!';
+}
 
-  if (Math.abs(e.value) >= gscroll.AXIS_THRESHOLD) {
-    console.log('Gamepad axis move at index %d: %s. Axis: %d. Value: %f.',
-      e.gamepad.index, e.gamepad.id, e.axis, e.value);
+var caf = window.cancelRequestAnimationFrame ||
+  window.mozCancelRequestAnimationFrame ||
+  window.webkitCancelRequestAnimationFrame;
+
+
+var gamepads = new window.Gamepads({
+  axisThreshold: 0,
+  indices: {
+    'standard': {
+      cursorX: 2,
+      cursorY: 3,
+      scrollX: 0,
+      scrollY: 1,
+      back: 9,
+      forward: 10,
+      zoomIn: 5,
+      zoomOut: 1
+    },
+    '46d-c216-Logitech Dual Action': {
+      cursorX: 3,
+      cursorY: 4,
+      scrollX: 1,
+      scrollY: 2,
+      back: 8,
+      forward: 9,
+      zoomIn: 7,
+      zoomOut: 6
+    },
+    '79-6-Generic   USB  Joystick': {
+      cursorX: null,
+      cursorY: null,
+      scrollX: 3,
+      scrollY: 2,
+      back: 6,
+      forward: 7,
+      zoomIn: 9,
+      zoomOut: 8
+    }
   }
 });
 
-exports.addEventListener('gamepadbuttondown', function (e) {
-  console.log('Gamepad button down at index %d: %s. Button: %d.',
-    e.gamepad.index, e.gamepad.id, e.button);
+gamepads.polling = false;
 
-  gcursor.onGamepadAxisMove(e);
-  gnav.onGamepadButtonDown(e);
-});
+if (gamepads.gamepadsSupported) {
+  gamepads.updateStatus = function () {
+    gamepads.polling = true;
+    gamepads.update();
+    raf(gamepads.updateStatus);
+  };
 
-exports.addEventListener('gamepadbuttonup', function (e) {
-  console.log('Gamepad button up at index %d: %s. Button: %d.',
-    e.gamepad.index, e.gamepad.id, e.button);
-});
+  gamepads.cancelLoop = function () {
+    gamepads.polling = false;
+
+    if (gamepads.nonFirefoxInterval) {
+      window.clearInterval(gamepads.nonFirefoxInterval);
+    }
+
+    caf(gamepads.updateStatus);
+  };
+
+
+  exports.addEventListener('gamepadconnected', function (e) {
+    console.log('Gamepad connected at index %d: %s. %d buttons, %d axes.',
+      e.gamepad.index, e.gamepad.id, e.gamepad.buttons.length, e.gamepad.axes.length);
+
+    gamepads.updateStatus();
+  });
+
+  exports.addEventListener('gamepaddisconnected', function (e) {
+    console.log('Gamepad removed at index %d: %s.', e.gamepad.index, e.gamepad.id);
+  });
+
+  if (gamepads.nonstandardEventsEnabled) {
+    exports.addEventListener('gamepadaxismove', function (e) {
+      gcursor.onGamepadAxisMove(e);
+      gscroll.onGamepadAxisMove(e);
+
+      if (Math.abs(e.value) >= gscroll.AXIS_THRESHOLD) {
+        console.log('Gamepad axis move at index %d: %s. Axis: %d. Value: %f.',
+          e.gamepad.index, e.gamepad.id, e.axis, e.value);
+      }
+    });
+
+    exports.addEventListener('gamepadbuttondown', function (e) {
+      console.log('Gamepad button down at index %d: %s. Button: %d.',
+        e.gamepad.index, e.gamepad.id, e.button);
+
+      gcursor.onGamepadAxisMove(e);
+      gnav.onGamepadButtonDown(e);
+    });
+
+    exports.addEventListener('gamepadbuttonup', function (e) {
+      console.log('Gamepad button up at index %d: %s. Button: %d.',
+        e.gamepad.index, e.gamepad.id, e.button);
+    });
+  }
+
+  // At the time of this writing, Firefox is the only browser that correctly
+  // fires the `gamepadconnected` event. For the other browsers
+  // <https://crbug.com/344556>, we start polling every 100ms until the
+  // first gamepad is connected.
+  if (window.Gamepads.utils.browser !== 'firefox') {
+    gamepads.nonFirefoxInterval = window.setInterval(function () {
+      if (gamepads.poll().length) {
+        gamepads.updateStatus();
+        window.clearInterval(gamepads.nonFirefoxInterval);
+      }
+    }, 100);
+  }
+}
+
+exports.gamepads = gamepads;
 
 })(window);
